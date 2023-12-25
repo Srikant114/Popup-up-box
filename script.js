d8e07b956aa4086372f9050af53424f9309d5d80
Srikant Sahu <srikantsahu114@gmail.com>
2023-12-25 19:15:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup");
+
+function showPopup() {
+  popup.classList.add("show-popup");
+}
+
+function hidePopup() {
+  popup.classList.remove("show-popup");
+}
